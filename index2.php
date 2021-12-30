2abf8a047eb37ff1946e689e62a69a29a2b48eab
martijnkleiweg <martijnkleiweg@hotmail.com>
2021-12-31 00:21:41 +0100

    Update 12/31

---
+++ index2.php
@@ -0,0 +1,44 @@
+<main>
+  <div class="list">
+
+<div class="gcheader">Buy discounted gift cards at Cardtraders</div>
+
+<div class="imagehome"><img src="giftcards.png"></div>
+
+<a href="buycards.php"><button class="btn-action">Buy Gift Cards</button></a>  <a href="sellcards.php"><button class="btn-action">Sell Gift Cards</button></a>
+
+
+<div class="gcheader">Popular gift cards</div>
+
+</div>
+
+<div class="center">
+
+<div class = "wrapper">
+
+<div class="card">
+<a href="buyamazonnl.php">
+<div class="rcorners"><img src="amazon-nl.jpg"></div>
+<div class="descr">Amazon NL (-10%)</div>
+</a>
+</div>
+
+<div class="card">
+<a href="buybolcom.php">
+<div class="rcorners"><img src="bol_com.jpg"></div>
+<div class="descr">Bol.com (-10%)</div>
+</a>
+</div>
+
+<div class="card">
+<a href="buybolcom.php">
+<div class="rcorners"><img src="adidas.svg"></div>
+<div class="descr">Adidas EU (-10%)</div>
+</a>
+</div>
+
+</div>
+</div>
+
+
+</main>
