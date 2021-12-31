c4421dfa06e73cba81dfcdc93dae2be3c7e6129d
martijnkleiweg <martijnkleiweg@hotmail.com>
2021-12-31 02:22:54 +0100

    Update 12/31

---
+++ buygiftcards2.php
@@ -0,0 +1,59 @@
+<main>
+
+
+
+  <h1>Buy Gift Cards</h1>
+
+<div class="center">
+
+<div class = "wrapper">
+
+<div class="card">
+<a href="buyamazonnl.php">
+<div class="rcorners"><img src="amazon-nl.jpg"></div>
+<div class="descr">Amazon NL (-10%)</div>
+</a>
+</div>
+
+<div class="card">
+<a href="buybolcom.php">
+<div class="rcorners"><img src="bol_com.jpg"></div>
+<div class="descr">Bol.com (-10%)</div>
+</a>
+</div>
+
+<div class="card">
+<a href="buybolcom.php">
+<div class="rcorners"><img src="adidas.svg"></div>
+<div class="descr">Adidas EU (-10%)</div>
+</a>
+</div>
+
+<div class="card">
+  <a href="buyamazonnl.php">
+<div class="rcorners"><img src="primark.svg"></div>
+<div class="descr">Primark (-15%)</div>
+</a>
+</div>
+
+<div class="card">
+  <a href="buyamazonnl.php">
+<div class="rcorners"><img src="netflix.svg"></div>
+<div class="descr">Netflix (-12%)</div>
+</a>
+</div>
+
+<div class="card">
+<a href="buybolcom.php">
+<div class="rcorners"><img src="zalando.svg"></div>
+<div class="descr">Zalando EU (-8%)</div>
+</a>
+</div>
+
+
+
+</div>
+
+</div>
+
+</main>
